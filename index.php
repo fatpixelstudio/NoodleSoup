7863badb332ff11788e41f388b02e533ca979e2d
Marijn Tijhuis <marijn@studiodumbar.com>
2014-07-20 10:19:32 +0200

    Fix loading of development/production styles and scripts.

--- index.php
+++ index.php
@@ -1,19 +1,24 @@
 <?php
 
 // Assets (dev vs. min+hash)
-// PHP read file
-$assets_css = $readfromdocroot . '/assets/stylesheets/min/hash.css.json', 'json');
-$assets_js = $readfromdocroot . '/assets/javascript/min/hash.js.json', 'json');
-// a::show($assets_css, $echo=true); // Show hash.json contents
-// a::show($assets_js, $echo=true); // Show hash.json contents
-if(c::get('environment') == 'local'):
+// Your site should live on it's own domain and should have file_get_contents
+// enabled to read the JSON file.
+$assets_css = $_SERVER['DOCUMENT_ROOT'] . '/assets/stylesheets/min/hash.css.json';
+$assets_js = $_SERVER['DOCUMENT_ROOT'] . '/assets/javascript/min/hash.js.json';
+
+$cssobj = json_decode(file_get_contents($assets_css));
+$jsobj = json_decode(file_get_contents($assets_js));
+
+if(strpos($_SERVER['SERVER_NAME'],'local.') !== false):
 	$env_suffix = 'dev';
-	$main_css = 'main.dev';
-	$print_css = 'print.dev';
+	$main_css = 'main';
+	$print_css = 'print';
+	$main_js = 'main.scripts';
 else:
 	$env_suffix = 'min';
-	$main_css = $getfromvar . ($assets_css, 'main');
-	$print_css = $getfromvar . ($assets_css, 'print');
+	$main_css = $cssobj->main;
+	$print_css = $cssobj->print;
+	$main_js = $jsobj->main;
 endif;
 
 ?><!DOCTYPE html>
@@ -37,18 +42,14 @@ endif;
 	<meta name="msapplication-TileImage" content="assets/images/favicon-144.png">
 	<meta name="msapplication-TileColor" content="#222222">
 
-	<link rel="stylesheet" href="assets/stylesheets/dev/<?php echo $env_suffix . '/' . $main_css ?>.css" />
-	<link rel="stylesheet" href="assets/stylesheets/dev/<?php echo $env_suffix . '/' . $print_css ?>.css" media="print" />
+	<link rel="stylesheet" href="assets/stylesheets/<?php echo $env_suffix . '/' . $main_css . '.' . $env_suffix ?>.css" />
+	<link rel="stylesheet" href="assets/stylesheets/<?php echo $env_suffix . '/' . $print_css . '.' . $env_suffix ?>.css" media="print" />
 
 	<?php /* Using Google Fonts? Uncomment this!
 	<link href="http://fonts.googleapis.com/css?family=Dosis:400,500,600" rel="stylesheet" type="text/css">
 	*/ ?>
 
-	<?php if($_SERVER['SERVER_NAME'] == 'local.domain.com'): ?>
-	<script src="assets/javascript/vendor/modernizr.min.js"></script>
-	<?php else: ?>
-	<script src="assets/javascript/vendor/modernizr.min.js"></script>
-	<?php endif; ?>
+	<script src="assets/javascript/vendor/modernizr.<?php echo $env_suffix; ?>.js"></script>
 </head>
 <body>
 	<!--[if lt IE 7]>
@@ -58,24 +59,19 @@ endif;
 	<p class="oldie-message">U gebruikt Internet Explorer 7 (een <strong>verouderd</strong> internetprogramma) om deze website te bekijken. <br /> <a href="http://browsehappy.com/">Download gratis een snellere en veiligere versie</a> om deze website optimaal te ervaren.</p>
 	<![endif]-->
 
-	<header>
-		<h1>Fat kit</h1>
+	<header class="Contain">
+		<h1>Stout Kit</h1>
 	</header>
-	<div role="main">
+	<div role="main" class="Contain Copy">
 		<p>A front-end starter kit by Fat Pixel.</p>
 	</div>
-	<footer role="contentinfo">
+	<footer role="contentinfo" class="Contain Copy">
 		<p>Dedication, baby!</p>
 	</footer>
 
-	<?php if($_SERVER['SERVER_NAME'] == 'local.domain.com'): ?>
-	<script src="assets/javascript/vendor/jquery.min.js"></script>
-	<script src="<?php bloginfo( 'stylesheet_directory' ); ?>assets/javascript/dev/main.scripts.dev.js"></script>
-	<?php else: ?>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="assets/javascript/vendor/jquery.min.js"><\/script>')</script>
-	<script src="assets/javascript/min/main.scripts.min.js"></script>
-	<?php endif; ?>
+	<script src="/assets/javascript/<?php echo $env_suffix . '/' . $main_js . '.' . $env_suffix ?>.js"></script>
 
 	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
